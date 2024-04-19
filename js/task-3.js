df01364d900a09474faf3cdb47b4d03572664bd0
Maksym Kosenko <maksym.kosenko2014@gmail.com>
2024-04-19 17:50:40 +0300

    add

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,16 @@
-function getElementWidth(content, padding, border) { 
-    const x =  Number.parseFloat(content);
-    const y =  Number.parseFloat(padding) * 2;
-    const z =  Number.parseFloat(border) * 2;
-    const borderBox = x + y + z;
-    return borderBox;
-};
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+function filterArray(numbers, value) {
+    let result = [];
+    for (const number of numbers) {
+        if (number > value) {
+            result.push(number);
+        };
+    };
+    return result;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
