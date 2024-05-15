1e72b51b98797a2906ae515c27f79d418c166887
Maksym Kosenko <maksym.kosenko2014@gmail.com>
2024-05-15 16:27:14 +0300

    add

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,10 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const liCategories = document.querySelectorAll(".item");
+console.log(`Namber of categories: ${liCategories.length}`);
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+liCategories.forEach((elem) => {
+    const itemTitel = elem.querySelector("h2").textContent;
+    const itemLi = elem.querySelectorAll("li").length;
+
+    console.log(`Category: ${itemTitel}`);
+    console.log(`Elements: ${itemLi}`);
+})
--- js/task-2.js
+++ js/task-2.js
@@ -1,31 +1,52 @@
-class Storage {
-  #item = [];
-  constructor(item) {
-    this.#item = item;
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
   }
+];
 
-  getItems() {
-    return this.#item;
-  }
+const listUl = document.querySelector(".gallery");
+listUl.style.display = `flex`;
+listUl.style.columnGap = `24px`;
+listUl.style.rowGap = `48px`;
+listUl.style.flexWrap = `wrap`;
+listUl.style.alignItems = `center`;
+listUl.style.listStyleType = `none`;
 
-  addItem(newItem) {
-    this.#item.push(newItem);
-  }
+const item = images.map((image) => {
+  const itemList = document.createElement("li");
+  const imageItem = document.createElement("img");
+  imageItem.src = image.url;
+  imageItem.alt = image.alt;
+  imageItem.width = 320;
+  imageItem.height = 200;
+  imageItem.style.borderRadius = `5%`;
+  imageItem.style.objectFit = `cover`;
+ 
+  itemList.append(imageItem);
+  return itemList;
+})
 
-  removeItem(itemToRemove) {
-   this.#item = this.#item.filter((elem) => elem !== itemToRemove);
-   
-  }
-}
 
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
-
-storage.removeItem("Scaner");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+listUl.append(...item);
